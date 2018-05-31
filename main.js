fa4ccebccb455f74463238381c4dd83e366c7719
xqn <379664090@qq.com>
2018-05-31 22:01:02 +0800

    开始写功能

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// var pageWidth = window.innerWidth;
+// if (pageWidth<860) {
+// 	var aside = document.getElementById("sidebar")
+// 	aside.style.left = '-300px';
+// 	var todo = document.getElementById("todos")
+// 	todo.style.paddingLeft = '0px';
+// 	var foot = document.getElementById("foot")
+// 	foot.style.paddingLeft = '0px';
+
+	// var style = document.createElement("style");
+	// style.type = "text.css";
+	// style.appendChild(document.createTextNode("aside{left: -300px;}"));
+	// var head = document.getElementsByTagName("head")[3];
+	// head.appendChild(style);
+}
