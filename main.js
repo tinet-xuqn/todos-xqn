d9e4001d0f536b25f544634f135b81885c679e0a
xqn <379664090@qq.com>
2018-06-05 09:26:09 +0800

    功能样式

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+// window.onresize = function() {
 
-// var pageWidth = window.innerWidth;
-// if (pageWidth<860) {
-// 	var aside = document.getElementById("sidebar")
-// 	aside.style.left = '-300px';
-// 	var todo = document.getElementById("todos")
-// 	todo.style.paddingLeft = '0px';
-// 	var foot = document.getElementById("foot")
-// 	foot.style.paddingLeft = '0px';
+// 	var pageWidth = window.innerWidth;
 
-	// var style = document.createElement("style");
-	// style.type = "text.css";
-	// style.appendChild(document.createTextNode("aside{left: -300px;}"));
-	// var head = document.getElementsByTagName("head")[3];
-	// head.appendChild(style);
-}
+// 	if (pageWidth<860) {
+// 		var aside = document.getElementById("sidebar")
+// 		aside.style.left = '-300px';
+// 		var todo = document.getElementById("todos")
+// 		todo.style.paddingLeft = '0px';
+// 		var foot = document.getElementById("foot")
+// 		foot.style.paddingLeft = '0px';
+// 	}
+
+// }
+var inputtext = document.querySelector("input");
+EventUtil.addHandler(inputtext,"keydown", function(event){
+	event = EventUtil.getEvent(event);
+	if (event.keyCode ==13) {
+		inputtext.value;
+	};
+});
+
+
